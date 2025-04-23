9239d7b6d557336f797915fcb28fd20e7a34bc7a
khadafi4 <khadaficromwell@icloud.com>
2025-04-23 12:12:03 -0400

    Create shipping.js

---
+++ models/shipping.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ShippingSchema = new mongoose.Schema({
+  shopperEmail: String,
+  destination: String,
+  carrier: String,
+  method: String,
+  cost: Number
+});
+
+module.exports = mongoose.model('Shipping', ShippingSchema);
