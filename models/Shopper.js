3469604be1be24173c0bb230e11cf8973470db65
khadafi4 <khadaficromwell@icloud.com>
2025-04-23 14:56:07 -0400

    Update Shopper.js

--- models/Shopper.js
+++ models/Shopper.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-
 const shopperSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String,
-  phone: String,
-  address: String
+    name: String,
+    age: Number,
+    email: String,
+    phone: String,
+    address: String
 });
-
 module.exports = mongoose.model('Shopper', shopperSchema, 'shoppers');
